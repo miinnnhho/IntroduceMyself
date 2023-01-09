9c8c8c52d123e9347554520b08ed21e79dca9e1d
miinnnhho <102772287+miinnnhho@users.noreply.github.com>
2023-01-09 13:50:30 +0900

    ver.1.1.0

--- scroll.js
+++ scroll.js
@@ -3,7 +3,7 @@ const sections = document.querySelectorAll("section")
 const firstTop = sections[0].offsetTop
 const secondTop = sections[1].offsetTop
 const thirdTop = sections[2].offsetTop
-const fourthTop = sections[3].offsetTop
+//const fourthTop = sections[3].offsetTop
 
 
 clicks[0].onclick = function() {
@@ -15,6 +15,6 @@ clicks[1].onclick = function() {
 clicks[2].onclick = function() {
   window.scroll({top: thirdTop, behavior: 'smooth'})
 }
-clicks[3].onclick = function() {
-  window.scroll({top: fourthTop, behavior: 'smooth'})
-}
+//clicks[3].onclick = function() {
+  //window.scroll({top: fourthTop, behavior: 'smooth'})
+//}
